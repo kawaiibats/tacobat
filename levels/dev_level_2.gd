730e2d82581e3ad5fda936f84d2adca51cdc76b0
kawaiibats <sharky2502@gmail.com>
2023-04-25 02:13:57 -0400

    Scene transition part 2/3
    Scene now fades in and out with an animation. The level calls its cleanup() function on leaving.

--- levels/dev_level_2.gd
+++ levels/dev_level_2.gd
@@ -6,18 +6,32 @@ signal level_changed(destination)
 @export var level_name = "devlevel2"
 
 
+func play_loaded_sound() -> void:
+	$LevelLoadedSound.play()
 
 
 
-	
-	
-	
 	
 func genesis():
 	print ("Dev Level Two Genesis Activating..")
 
 
+func cleanup():
+	print("cleanup2")
+	if $WarpAreaEnterSound.playing:
+		print("cleanup3")
+		await $WarpAreaEnterSound.finished
+	print("cleanup4")
+	queue_free()
+
+
+
 
 func _on_warp_area_warp_area_entered(destination):
 	print("dev level 2 print")
+	warp(destination)
+
+
+func warp(destination):
+	$WarpAreaEnterSound.play()
 	emit_signal("level_changed", destination)
