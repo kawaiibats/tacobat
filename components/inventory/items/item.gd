8ee476e81ab708709e3af2ee9601c390590deec6
kawaiibats <sharky2502@gmail.com>
2023-06-27 16:31:31 -0400

    item info big update
    Items have rarity, lore, family, and so on
    All of this displays within the item info box according to the item's specifications
    
    game enums have been updated appropriately

--- components/inventory/items/item.gd
+++ components/inventory/items/item.gd
@@ -6,16 +6,45 @@ class_name Item extends TextureRect
 # "Red Mushroom"
 @export var item_name : String 
 
+# "Considered especially tasty by many foodies across the Isles."
+@export var lore : String
 
 
-# Foragables are found on the ground and in bushes and other objects.
-# Seeds are found in trees bushes and other objects. They can also be purchased.
-# Crops are obtained from plants that are grown at the home.
-# Resources are found when objects are destroyed such as trees or ore rocks.
-# Tools are usable items which cannot be stacked
-# Equipment are placed in special inventory slots and give bonuses
+# OVERRIDES THE WORD "FORAGABLE", "CROP", or "SEEDS" for a more intricate feel
+# Only used by some. If there is no family it will say the type instead.
+@export var family : String = ""
+
+
+
+
+
+# FORAGY - Foragables are found on the ground and in bushes and other objects.
+# SEED - Seeds are found in trees bushes and other objects. They can also be purchased.
+# CROP - Crops are obtained from plants that are grown at the home.
+# RESOURCE - Resources are found when objects are destroyed such as trees or ore rocks.
+# TOOL - Tools are usable items which cannot be stacked
+# EQUIPMENT_HEAD, EQUIPMENT_TORSO, EQUIPMENT_BOTTOM, EQUIPMENT_FEET, EQUIPMENT_TRINKET - Equipment are placed in special inventory slots and give bonuses
 
 @export var type : Game_Enums.ITEM_TYPE
+
+
+
+
+
+
+
+
+# RARITY IS ONLY USED BY FORAGYS, SEEDS, CROPS, MAYBE RESOURCES
+# "None", Common", "Uncommon", "Rare", "Exceptional", "Exotic"
+@export var rarity : Game_Enums.ITEM_RARITY
+
+
+
+
+
+
+
+
 
 
 func _ready():
--- components/inventory/items/item_info.gd
+++ components/inventory/items/item_info.gd
@@ -1,12 +1,108 @@
 extends NinePatchRect
 
-@export var item_path: NodePath
-@onready var item = get_node( item_path ) 
+
+# "Red Mushroom" - ITEM NAME
+@export var display_name_path: NodePath
+@onready var display_name = get_node( display_name_path ) 
+
+
+# "Common" + " " + "Foragy" -- ITEM RARITY (if there is one) + ITEM TYPE
+# Coloured depending on the rarity
+@export var tagline_path: NodePath
+@onready var tagline = get_node( tagline_path )
+
+
+
+@export var lore_path: NodePath
+@onready var lore = get_node( lore_path )
+
+
+
+
 
 func display( slot : Inventory_Slot ):
 	self.global_position = slot.size + slot.global_position
 	
-	item.text = slot.item.item_name
+	# SET DISPLAY NAME
+	display_name.text = slot.item.item_name
+	
+	
+	
+	
+	# SET DISPLAY TAGLINE (RARITY + TYPE)
+	
+	var tagText = ""
+	
+	# CHECK FOR RARITY, DISPLAY IT, CHANGE COLOUR
+	match slot.item.rarity:
+		0:
+			tagline.label_settings.font_color = Color(0.67,0.31,0.41)
+			
+		1 : 
+			tagText += "Common "
+			tagline.label_settings.font_color = Color.FOREST_GREEN
+			
+		2 : 
+			tagText += "Uncommon "
+			tagline.label_settings.font_color = Color.AQUAMARINE
+			
+		3 : 
+			tagText += "Rare "
+			tagline.label_settings.font_color = Color.DARK_SLATE_BLUE
+		
+		4 : 
+			tagText += "Exceptional "
+			tagline.label_settings.font_color = Color.VIOLET
+		
+		5 : 
+			tagText += "Exotic "
+			tagline.label_settings.font_color = Color.GOLDENROD
+			
+	
+	
+	# ADD TYPE AND DISPLAY TAGLINE
+	match slot.item.type:
+		0 :
+			if slot.item.family == "":
+				tagText += "Foragable"
+			else:
+				tagText += slot.item.family
+		1 :
+			if slot.item.family == "":
+				tagText += "Seeds"
+			else:
+				tagText += slot.item.family
+		2 :
+			if slot.item.family == "":
+				tagText += "Crop"
+			else:
+				tagText += slot.item.family
+		3 :
+			if slot.item.family == "":
+				tagText += "Resource"
+			else:
+				tagText += slot.item.family
+		4 :
+			tagText += "Tool"
+		5 :
+			tagText += "Headgear"
+		6 :
+			tagText += "Chestwear"
+		7 :
+			tagText += "Bottomwear"
+		8 :
+			tagText += "Footwear"
+		9 :
+			tagText += "Trinket"
+
+	tagline.text = tagText
+	
+	
+	
+	
+	# SET DISPLAY LORE
+	lore.text = slot.item.lore
+	
 	
 	show()
 
